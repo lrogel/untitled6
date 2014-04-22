5c1b29876c530fc0a3d623443b0f750229ca3158
Lucero <lrogel@ucsc.edu>
2014-04-21 21:16:17 -0700

    The single run simulation for the function monte_finances. Objectivewas to get the ratio between the new current balance and the old current balance based on an investment.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#Lucero Rogel
+#lrogel@ucsc.edu
+#CMPS 5P
+#Assignment2 
+
+import random
+import math
+
+#Simple program to observe how stock and investment portfolios behave.
+#Single run of the monte carlo simulation to obtain the ratio.
+#ratio= between new current balance and what you originally had in the account (current balance.
+
+
+def monte_finances(avg_return,stddev_return,leverage_ratio,num_of_periods):
+    current_balance=1000
+    for i in range(num_of_periods):
+        distribution_number = random.normalvariate(avg_return,stddev_return)
+        rate_of_return = distribution_number *(leverage_ratio * current_balance)
+        current_balance = rate_of_return+current_balance
+        if current_balance <=0:
+           current_balance=0
+        ratio = current_balance/1000
+        print ratio
+print monte_finances(12,5,6,5)
